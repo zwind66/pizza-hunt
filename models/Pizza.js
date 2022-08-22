9a4d535a81bae6b72fe7b6b45d9c5c2d9407685e
Shuai Zuo <szuo1@vols.utk.edu>
2022-08-21 21:03:07 -0500

    done

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,25 +1,55 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
-        type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdBy: {
-        type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
     },
     size: {
-        type: String,
-        default: 'Large'
+      type: String,
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
+      default: 'Large'
     },
-    toppings: []
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
 });
 
-// create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
-// export the Pizza model
 module.exports = Pizza;
--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -62,11 +62,12 @@ const handlePizzaSubmit = event => {
   })
     .then(response => response.json())
     .then(postResponse => {
-      alert('Pizza created successfully!');
       console.log(postResponse);
     })
     .catch(err => {
       console.log(err);
+      saveRecord(formData);
+      // DO INDEXED DB STUFF HERE
     });
 };
 
